12d56b0c550a629696d70c71489e213b3214782e
shashankpr <s.p.rao@student.tudelft.nl>
2017-05-15 03:52:43 +0200

    Added one hot encoding to preprocessing pipeline

--- Week1/preprocess.py
+++ Week1/preprocess.py
@@ -2,11 +2,15 @@ import pandas as pd
 import numpy as np
 import logging
 
+from label_encoder import EncodeLabel
+
 logging.basicConfig(filename='logs/process.log', filemode='w+', level=logging.DEBUG)
 
 class PreprocessData(object):
     def __init__(self):
         self.filename = 'data_for_student_case.csv'
+        self.cols_to_convert = ['issuercountrycode', 'txvariantcode', 'currencycode', 'shoppercountrycode',
+                                'shopperinteraction', 'cardverificationcodesupplied', 'accountcode', 'cvcresponsecode']
 
     def _load_dataset(self):
         fraud_df = pd.read_csv(self.filename)
@@ -54,6 +58,35 @@ class PreprocessData(object):
 
         return clean_df_non_ref
 
-if __name__ == '__main__':
-    p = PreprocessData()
-    p.remove_refused(save_file=True)
+    def encode_cat_to_labels(self):
+        clean_df = self.remove_refused()
+        e = EncodeLabel()
+        clean_df_one_hot = e.encode_one_hot(clean_df=clean_df, cols=self.cols_to_convert)
+
+        return clean_df_one_hot
+
+    def encode_target_label(self, target_col):
+        e = EncodeLabel()
+        target_labels = e.encode_label(target_col)
+
+        return target_labels
+
+    def create_dataframe(self):
+
+        clean_df_one_hot = self.encode_cat_to_labels()
+        target_col = clean_df_one_hot['simple_journal']
+        amount_col = clean_df_one_hot['amount']
+
+        encoded_features = clean_df_one_hot.ix[:, 7:]
+        target_labels = self.encode_target_label(target_col)
+        # encoded_features['target'] = target_labels
+        encoded_features['amount'] = amount_col
+
+        logging.debug("DataFrame Info:{} ".format(encoded_features.info()))
+        # logging.info("First 5 entries: {}".format(encoded_features.head()))
+
+        return encoded_features, target_labels
+
+# if __name__ == '__main__':
+#     p = PreprocessData()
+#     p.create_dataframe()
