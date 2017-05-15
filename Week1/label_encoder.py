26ab9e36c69eff5fad204a0d5593e5febc63f9b2
shashankpr <s.p.rao@student.tudelft.nl>
2017-05-15 03:53:29 +0200

    major refactoring

--- Week1/label_encoder.py
+++ Week1/label_encoder.py
@@ -5,30 +5,23 @@ from sklearn import preprocessing
 
 
 class EncodeLabel(object):
-    def __init__(self, filename):
-        self.cols_to_convert = ['issuercountrycode', 'txvariantcode', 'currencycode', 'shoppercountrycode',
-                                'shopperinteraction', 'cardverificationcodesupplied', 'accountcode', 'cvcresponsecode']
-
-        self.filename = filename
-
-    def _load_data(self):
-        clean_df = pd.read_csv(self.filename)
-        return clean_df
-
-    def encode_one_hot(self):
-        clean_df = self._load_data()
-        one_hot_df = pd.get_dummies(clean_df, columns=self.cols_to_convert)
+    @staticmethod
+    def encode_one_hot(clean_df, cols):
+        # clean_df = self.load_data()
+        one_hot_df = pd.get_dummies(clean_df, columns=cols)
 
         return one_hot_df
 
-    def encode_binary_label(self, target_col):
+    @staticmethod
+    def encode_binary_label(target_col):
         lb = preprocessing.LabelBinarizer()
         lb.fit(target_col)
 
         transform_label = lb.transform(target_col)
         return transform_label
 
-    def encode_label(self, columns):
+    @staticmethod
+    def encode_label(columns):
         lb = preprocessing.LabelEncoder()
         transform_label = lb.fit_transform(columns)
         return transform_label
